70a4dc22876ec42d98d7188ce8631cfb18bb30fb
Gosha Arinich <me@goshakkk.name>
2014-06-07 00:08:46 +0300

    small refactor

--- src/konin/core.clj
+++ src/konin/core.clj
@@ -10,22 +10,35 @@
 (defn bytes->map [^bytes payload]
   (-> payload (String. "UTF-8") (json/read-str :key-fn keyword)))
 
+(defn gen-queue-name [prefix]
+  (str prefix "_rpc_queue"))
+
 (defn make-basic-server [prefix]
-  (let [queue-name (str prefix "_rpc_queue")
+  (let [queue-name (gen-queue-name prefix)
         conn (rmq/connect)
-        ch (lch/open conn)
-        q (lq/declare ch queue-name)]
-    {:conn conn :ch ch :q q :queue-name queue-name}))
+        ch (lch/open conn)]
+    (lq/declare ch queue-name)
+    {:conn conn :ch ch :queue-name queue-name}))
+
+(defn apply-fn [{:keys [interface function args]} handler-map]
+  (let [iface-fn (keyword interface)
+        f-fn (keyword function)]
+    (-> handler-map
+        iface-fn
+        f-fn
+        (apply args))))
+
+(defn wrap-result [result]
+  {:result result})
 
 (defn server-message-handler [handler-map]
   (fn [ch {:keys [reply-to correlation-id]} payload]
-    (let [{:keys [interface function args]} (bytes->map payload)
-          iface-fn (keyword interface)
-          f-fn (keyword function)
-          f (-> handler-map iface-fn f-fn)
-          result (apply f args)
-          r (json/write-str {:result result})]
-      (lb/publish ch "" reply-to r :correlation-id correlation-id))))
+    (let [data (bytes->map payload)
+          result (-> data
+                     (apply-fn handler-map)
+                     wrap-result
+                     json/write-str)]
+      (lb/publish ch "" reply-to result :correlation-id correlation-id))))
 
 (defn start-server-consumer [serv handler-map]
   (lc/subscribe (:ch serv) (:queue-name serv) (server-message-handler handler-map))
